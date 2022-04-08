b872de914227e0ed5c40c7f5d9717186266a3198
DESKTOP-O1U24RQ\Kevin_Yu <jsyu17@gmail.com>
2022-04-08 17:32:56 +0800

    add sample

---
+++ sample/SendMailSample/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using FluentEmail.Core;
+using FluentEmail.Core.Interfaces;
+using FluentEmail.Graph;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace SendMailSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var services = new ServiceCollection();
+            services.AddFluentEmail("user@user.com").AddGraphSender(Initial());
+            services.AddScoped<IFluentEmail, Email>();
+            var provider = services.BuildServiceProvider();
+
+            //get mail service
+            var eamil = provider.GetRequiredService<IFluentEmail>();
+            var sender = provider.GetRequiredService<ISender>();
+            eamil.Sender = sender;
+
+
+            eamil.SetFrom("user@user.com", "testGuy")
+                    .To("user@hotmail.com")
+                    .Subject("Test contact Email ")
+                    .Body(GetTestMailBody());
+            eamil.Data.IsHtml = true;
+            var response = eamil.Send();
+            Console.ReadKey();
+
+        }
+
+        private static GraphSenderOptions Initial()
+        {
+            var config = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+
+                .Build();
+            var graphSenderOptions = config
+                .GetSection("GraphSenderOptions")
+                .Get<GraphSenderOptions>();
+
+            return graphSenderOptions;
+        }
+
+        private static string GetTestMailBody()
+        {
+            return @"<!DOCTYPE html>
+<html>
+<head>
+    <meta charset=""utf-8"" />
+    <title></title>
+</head>
+<body>
+Test mail ok<br/>
+<b>Bold text</b>
+</body>
+</html>";
+        }
+    }
+}
