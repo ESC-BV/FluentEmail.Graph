90e739c32dad4f7e956cb899bff5faaef620a1e5
David De Sloovere <david.desloovere@esc.be>
2022-09-14 13:20:25 +0200

    config

--- sample/SendMailTestApp/Program.cs
+++ sample/SendMailTestApp/Program.cs
@@ -5,55 +5,77 @@ using FluentEmail.Core.Interfaces;
 using FluentEmail.Graph;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
 
-var builder = Host.CreateDefaultBuilder(args)
-    .ConfigureServices(
-        (hostContext, services) =>
-        {
-            var graphSenderOptions = hostContext.Configuration.GetRequiredSection("GraphSenderOptions")
-                .Get<GraphSenderOptions>();
-            var fromAddress = hostContext.Configuration.GetValue<string>("MailOptions:FromAddress");
+namespace SendMailTestApp;
 
-            services.AddScoped<IFluentEmail, Email>();
-            services.AddFluentEmail(fromAddress)
-                .AddGraphSender(graphSenderOptions);
-        });
+using System.Reflection;
 
-using var host = builder.Build();
-
-host.
-// get mail service and out Graph sender
-var email = host.Services.GetService<IFluentEmail>();
-var sender = host.Services.GetService<ISender>();
-if (sender is not GraphSender)
+class Program
 {
-    Console.WriteLine("GraphSender not resolved.");
-
-    return;
-}
-
-Console.WriteLine("Enter destination e-mail address to send test mail to:");
-var destinationAddress = Console.ReadLine();
-email.To(destinationAddress)
-    .Subject("Test Email Graph API")
-    .Body("This is the <b>body</b> of the mail.");
-email.Data.IsHtml = true;
-
-var response = await sender.SendAsync(email);
-if (response.Successful)
-{
-    Console.WriteLine("Mail sent.");
-}
-else
-{
-    Console.WriteLine("Mail was NOT sent.");
-    foreach (var errorMessage in response.ErrorMessages)
+    static async Task Main(string[] args)
     {
-        Console.WriteLine("- " + errorMessage);
+        var configurationRoot = GetConfigurationRoot(args);
+        var graphSenderOptions = configurationRoot.GetRequiredSection("GraphSenderOptions")
+            .Get<GraphSenderOptions>();
+        var fromAddress = configurationRoot.GetValue<string>("MailOptions:FromAddress");
+
+        var services = new ServiceCollection();
+        services.AddScoped<IFluentEmail, Email>();
+        services.AddFluentEmail(fromAddress)
+            .AddGraphSender(graphSenderOptions);
+        var serviceProvider = services.BuildServiceProvider();
+
+        // get mail service and out Graph sender
+        var email = serviceProvider.GetRequiredService<IFluentEmail>();
+        var sender = serviceProvider.GetRequiredService<ISender>();
+        if (sender is not GraphSender)
+        {
+            Console.WriteLine("GraphSender not resolved.");
+
+            return;
+        }
+
+        Console.WriteLine("Enter destination e-mail address to send test mail to:");
+        var destinationAddress = Console.ReadLine();
+        email.To(destinationAddress)
+            .Subject("Test Email Graph API")
+            .Body("This is the <b>body</b> of the mail.");
+        email.Data.IsHtml = true;
+
+        var response = await sender.SendAsync(email);
+        if (response.Successful)
+        {
+            Console.WriteLine("Mail sent.");
+        }
+        else
+        {
+            Console.WriteLine("Mail was NOT sent.");
+            foreach (var errorMessage in response.ErrorMessages)
+            {
+                Console.WriteLine("- " + errorMessage);
+            }
+        }
+
+        Console.ReadKey();
+    }
+
+    private static IConfigurationRoot GetConfigurationRoot(string[] args)
+    {
+        var config = new ConfigurationBuilder();
+        config.AddJsonFile("appsettings.json", optional: true);
+
+        var appAssembly = Assembly.Load(new AssemblyName("SendMailTestApp"));
+        config.AddUserSecrets(appAssembly, optional: false);
+
+        config.AddEnvironmentVariables();
+
+        if (args is { Length: > 0 })
+        {
+            config.AddCommandLine(args);
+        }
+
+        var configurationRoot = config.Build();
+
+        return configurationRoot;
     }
 }
-
-Console.ReadKey();
-
-await host.RunAsync();
