d9e2ac92911723d9a99b0a732806f4dba5f09e38
David De Sloovere <david.desloovere@esc.be>
2023-04-07 09:40:25 +0200

    🔀 chore: upgrade Microsoft.Graph to v5 (#178)
    * chore: update dependencies
    
    * version

--- src/FluentEmail.Graph/FluentEmailServicesBuilderExtensions.cs
+++ src/FluentEmail.Graph/FluentEmailServicesBuilderExtensions.cs
@@ -1,44 +1,43 @@
-﻿namespace FluentEmail.Graph
+﻿namespace FluentEmail.Graph;
+
+using FluentEmail.Core.Interfaces;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.Graph;
+
+/// <summary>
+/// Contains extension methods to register the <see cref="GraphSender"/> with the <c>FluentEmailServicesBuilder</c> from <c>FluentEmail.Core</c>.
+/// </summary>
+public static class FluentEmailServicesBuilderExtensions
 {
-    using FluentEmail.Core.Interfaces;
-    using Microsoft.Extensions.DependencyInjection;
-    using Microsoft.Extensions.DependencyInjection.Extensions;
-    using Microsoft.Graph;
-
-    /// <summary>
-    /// Contains extension methods to register the <see cref="GraphSender"/> with the <c>FluentEmailServicesBuilder</c> from <c>FluentEmail.Core</c>.
-    /// </summary>
-    public static class FluentEmailServicesBuilderExtensions
+    public static FluentEmailServicesBuilder AddGraphSender(
+        this FluentEmailServicesBuilder builder,
+        GraphSenderOptions options)
     {
-        public static FluentEmailServicesBuilder AddGraphSender(
-            this FluentEmailServicesBuilder builder,
-            GraphSenderOptions options)
-        {
-            builder.Services.TryAdd(ServiceDescriptor.Scoped<ISender>(_ => new GraphSender(options)));
-            return builder;
-        }
+        builder.Services.TryAdd(ServiceDescriptor.Scoped<ISender>(_ => new GraphSender(options)));
+        return builder;
+    }
 
-        public static FluentEmailServicesBuilder AddGraphSender(
-            this FluentEmailServicesBuilder builder,
-            string graphEmailClientId,
-            string graphEmailTenantId,
-            string graphEmailSecret)
+    public static FluentEmailServicesBuilder AddGraphSender(
+        this FluentEmailServicesBuilder builder,
+        string graphEmailClientId,
+        string graphEmailTenantId,
+        string graphEmailSecret)
+    {
+        var options = new GraphSenderOptions
         {
-            var options = new GraphSenderOptions
-            {
-                ClientId = graphEmailClientId,
-                TenantId = graphEmailTenantId,
-                Secret = graphEmailSecret,
-            };
-            return builder.AddGraphSender(options);
-        }
+            ClientId = graphEmailClientId,
+            TenantId = graphEmailTenantId,
+            Secret = graphEmailSecret,
+        };
+        return builder.AddGraphSender(options);
+    }
 
-        public static FluentEmailServicesBuilder AddGraphSender(
-            this FluentEmailServicesBuilder builder,
-            GraphServiceClient graphClient)
-        {
-            builder.Services.TryAdd(ServiceDescriptor.Scoped<ISender>(_ => new GraphSender(graphClient)));
-            return builder;
-        }
+    public static FluentEmailServicesBuilder AddGraphSender(
+        this FluentEmailServicesBuilder builder,
+        GraphServiceClient graphClient)
+    {
+        builder.Services.TryAdd(ServiceDescriptor.Scoped<ISender>(_ => new GraphSender(graphClient)));
+        return builder;
     }
 }
--- tests/FluentEmail.Graph.Tests/UnitTest1.cs
+++ tests/FluentEmail.Graph.Tests/UnitTest1.cs
@@ -1,23 +1,22 @@
-namespace FluentEmail.Graph.Tests
+namespace FluentEmail.Graph.Tests;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+[TestClass]
+public class UnitTest1
 {
-    using Microsoft.VisualStudio.TestTools.UnitTesting;
-
-    [TestClass]
-    public class UnitTest1
+    [TestMethod]
+    public void TestMethod1()
     {
-        [TestMethod]
-        public void TestMethod1()
-        {
-            // no tests yet, bit hard because you need actual appid, tenantid and secret for integration test
-            ////// arrange
-            ////const int Expected = 4;
-            ////var calculator = new Class1();
+        // no tests yet, bit hard because you need actual appid, tenantid and secret for integration test
+        ////// arrange
+        ////const int Expected = 4;
+        ////var calculator = new Class1();
 
-            ////// act
-            ////var actual = calculator.Add(2, 2);
+        ////// act
+        ////var actual = calculator.Add(2, 2);
 
-            ////// assert
-            ////actual.Should().Be(Expected);
-        }
+        ////// assert
+        ////actual.Should().Be(Expected);
     }
 }
